edf61073a017185bd125057451b8f25811af4b86
Kyle Hoondert <kyle.hoondert@imply.io>
2023-04-20 12:15:58 +0100

    Adding latest commits

---
+++ src/python/collectors/ClusterAPICollector.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+
+import requests
+import argparse
+import sys
+import tempfile
+import zipfile
+import json
+import os
+import datetime
+
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+def checkOptions():
+    parser = argparse.ArgumentParser(description = 'Getafix - Cluster API Info Collector')
+    parser.add_argument('Router', action='store', type=str, help='Router URL with protocol and port')
+    parser.add_argument('-k', '--kerberos', action='store_true', dest='kerberos', help='''
+                        Enable kerberos authentication (must already have valid ticket''')
+    parser.add_argument('-u', '--user', action='store', type=str, dest='user', help='Username')
+    parser.add_argument('-p', '--password', action='store', type=str, dest='password', help='Password')
+    parser.add_argument('-t', '--token', action='store', type=str, dest='token', help='Token (like YRtWa... - no "Basic"')
+    options = parser.parse_args()
+
+    if options.kerberos:
+        from requests_kerberos import HTTPKerberosAuth, REQUIRED
+        global kerberos_auth
+        kerberos_auth = HTTPKerberosAuth(mutual_authentication=REQUIRED, sanitize_mutual_error_response=False)
+    
+    if options.token:
+        options.auth = True
+    else:
+        options.auth = False
+
+    if options.user:
+        if not options.password:
+            print('Please enter username and password, or token')
+            sys.exit(2)
+        else:
+            import base64
+            options.auth = True
+            userPass = bytes(options.user + ':' + options.password, encoding='utf-8')
+            options.token = base64.b64encode(userPass).decode("utf-8")
+    
+    return options
+
+
+def getRequest(URL, options):
+    fullURL = options.Router + URL
+    if options.kerberos:
+        req = requests.get(fullURL, auth=kerberos_auth, verify=False)
+
+    elif options.auth:
+        headers = {'Authorization': 'Basic ' + options.token}
+        req = requests.get(fullURL, headers=headers, verify=False)
+
+    else:
+        req = requests.get(fullURL, verify=False)
+    try:
+        JSON = req.json()
+    except json.decoder.JSONDecodeError:
+        JSON = {}
+    return JSON
+    
+
+def postRequest(URL, query, options):
+    fullURL = options.Router + URL
+    if options.kerberos:
+        headers = {'Content-Type': 'application/json'}
+        req = requests.post(fullURL, headers=headers, json=qery, auth=kerberos_auth, verify=False)
+
+    elif options.auth:
+        headers = {'Content-Type': 'application/json', 'Authorization': 'Basic ' + options.token}
+        req = requests.post(fullURL, headers=headers, json=query, verify=False)
+    else:
+        headers = {'Content-Type': 'application/json'}
+        req = requests.post(fullURL, headers=headers, json=query, verify=False)
+
+    JSON = req.json()
+    return JSON
+
+
+def getSegments(options):
+    queryJSON = {'query': """
+        SELECT "datasource", count("segment_id") as segmentCount, AVG("size") as avgSize, avg("num_rows") as avgRows 
+        FROM sys.segments  
+        GROUP BY "datasource"
+        """
+    }
+    URL = '/druid/v2/sql'
+    return postRequest(URL, queryJSON, options)
+
+
+def getServers(options):
+    queryJSON = {'query': """
+        SELECT "server" AS "service", "server_type", "tier", "host", "plaintext_port", "tls_port", "curr_size", "max_size"
+        FROM sys.servers 
+        ORDER BY "service" DESC
+        """
+    }
+    URL = '/druid/v2/sql'
+    return postRequest(URL, queryJSON, options)
+
+
+def getCompaction(options):
+    URL = '/druid/coordinator/v1/config/compaction'
+    return getRequest(URL, options)
+
+
+def getCompactionStatus(options):
+    URL = '/druid/coordinator/v1/compaction/status'
+    return getRequest(URL, options)
+
+
+def getSupervisors(options):
+    URL = '/druid/indexer/v1/supervisor?full'
+    return getRequest(URL, options)
+
+
+def getTasks(options):
+    queryJSON = {'query': """
+        WITH tasks AS (
+          SELECT "task_id", "group_id", "type", "datasource", "created_time", "location", "duration", "error_msg",
+          CASE
+            WHEN "error_msg" = 'Shutdown request from user' THEN 'CANCELED' 
+            WHEN "status" = 'RUNNING' THEN "runner_status" 
+            ELSE "status" 
+          END AS "status"
+          FROM sys.tasks
+        )
+        SELECT "task_id", "group_id", "type", "datasource", "created_time", "location", "duration", "error_msg", "status"
+        FROM tasks
+        ORDER BY "created_time" DESC
+        """
+        }
+    URL = '/druid/v2/sql'
+    return postRequest(URL, queryJSON, options)
+
+
+def getRetention(options):
+    URL = '/druid/coordinator/v1/rules?full'
+    return getRequest(URL, options)
+
+
+def getDatasources(options):
+    URL = '/druid/coordinator/v1/metadata/datasources?full'
+    return getRequest(URL, options)
+
+
+def getCoordinatorSettings(options):
+    URL = '/druid/coordinator/v1/config'
+    return getRequest(URL, options)
+
+
+def getOverlordSettings(options):
+    URL = '/druid/indexer/v1/worker'
+    return getRequest(URL, options)
+
+
+def getLookups(options):
+    URL = '/druid/coordinator/v1/lookups/config/all'
+    return getRequest(URL, options)
+
+
+def getWorkers(options):
+    URL = '/druid/indexer/v1/workers'
+    return getRequest(URL, options)
+    
+def collectAPIData(options):
+        json.dumps(getSegments(options))
+        json.dumps(getCompaction(options))
+        json.dumps(getCompactionStatus(options))
+        json.dumps(getSupervisors(options))
+        json.dumps(getTasks(options))
+        json.dumps(getRetention(options))
+        json.dumps(getDatasources(options))
+        json.dumps(getCoordinatorSettings(options))
+        json.dumps(getOverlordSettings(options))
+        json.dumps(getServers(options))
+        json.dumps(getLookups(options))
+        json.dumps(getWorkers(options))
+
+def main():
+    options = checkOptions()
+    collectAPIData(options)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/python/processors/BrokerLogParser.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+import sys
+import csv
+import json
+import argparse
+import datetime
+
+csv.field_size_limit(sys.maxsize)
+
+def checkOptions():
+    parser = argparse.ArgumentParser(description = 'Broker log query parser')
+    parser.add_argument('-i', '--inputFile', required=True, action='store', type=str, dest='inputFile', help='Source Broker log file')
+    parser.add_argument('-o', '--outputFile', required=True, action='store', dest='outputFile', help='Output csv file')
+    options = parser.parse_args()
+
+    return options
+
+def main():
+    global options
+    options = checkOptions()
+
+    with open(options.inputFile, "r") as inFile:
+        with open(options.outputFile, "w") as outFile:
+            writer = csv.writer(outFile)
+            writer.writerow(["eventtime", "querytype", "datasource", "queryid", "priority", "recency", "duration", "queryTime", "queryBytes", "success", "filters", "query"])
+            tsv_file = csv.reader(inFile, delimiter = "\t")
+            for inLine in tsv_file:
+                try:
+                    query = json.loads(inLine[2])
+                    queryResult = json.loads(inLine[3])
+                except:
+                    continue
+                try:
+                     logTime = inLine[0].split()[5]
+                     logDate = datetime.datetime.strptime(logTime, "%Y-%m-%dT%H:%M:%S.%fZ")
+                except:
+                     continue
+                if query['queryType'] == 'segmentMetadata':
+                    continue
+                elif query['queryType'] == 'union':
+                    dataSource = "'" + query['query']['dataSource']['name'] + "'"
+                    queryId = query['query']['context']['queryId']
+                    try:
+                        priority = query['query']['context']['priority']
+                    except:
+                        priority = 'Null'
+                    intervals = query['query']['intervals']['intervals']
+                else:
+                    if query['dataSource']['type'] == 'table':
+                        try:
+                            dataSource = query['dataSource']['name']
+                        except:
+                            dataSource = ''
+                            for ds in query['dataSource']['dataSources']:
+                                if len(dataSource) > 1:
+                                    dataSource = dataSource + ",'" + ds['name'] + "'"
+                                else:
+                                    dataSource = dataSource + "'" + ds['name'] + "'"
+                    else:
+                        dataSource = 'inline'
+                    queryId = query['context']['queryId']
+                    try:
+                        priority = query['context']['priority']
+                    except:
+                        priority = 'Null'
+                    intervals = query['intervals']['intervals']
+                interval = intervals[0]
+                filterList = []
+                try:
+                    if query['filter']['type'] == 'selector' or query['filter']['type'] == 'regex':
+                        filterList.append(query['filter']['dimension'].replace('"',''))
+                    elif query['filter']['type'] == 'columnComparison':
+                        filterList.append(query['filter']['dimensions'].replace('"',''))
+                    elif query['filter']['type'] == 'and' or query['filter']['type'] == 'or':
+                        for filters in query['filter']['fields']:
+                            filterList.append(filters['dimension'].replace('"',''))
+                except:
+                    continue
+                #print(str(filterList))
+                try:
+                    intervalStart = datetime.datetime.strptime(interval.split('/')[0], "%Y-%m-%dT%H:%M:%S.%fZ")
+                except ValueError:
+                    intervalStart = datetime.datetime.strptime('0001-01-01T00:00:00.000Z', "%Y-%m-%dT%H:%M:%S.%fZ")
+                try:
+                    intervalEnd = datetime.datetime.strptime(interval.split('/')[1], "%Y-%m-%dT%H:%M:%S.%fZ")
+                except ValueError:
+                    intervalEnd = datetime.datetime.strptime('9999-12-31T23:59:59.999Z', "%Y-%m-%dT%H:%M:%S.%fZ")
+                duration = intervalEnd - intervalStart
+                recency = logDate - intervalStart
+                queryTime = queryResult['query/time']
+                queryBytes = queryResult['query/bytes']
+                success = queryResult['success']
+                #outLine = [ logTime, query['queryType'], dataSource, queryId, priority, int(round(recency.total_seconds())), int(round(duration.total_seconds())), queryTime, queryBytes, success, str(filterList), json.dumps(query)]
+                writer.writerow([logTime, query['queryType'], dataSource, queryId, priority, int(round(recency.total_seconds())), int(round(duration.total_seconds())), queryTime, queryBytes, success, (', '.join(filterList)), json.dumps(query)])
+
+if __name__ == '__main__':
+    main()   
