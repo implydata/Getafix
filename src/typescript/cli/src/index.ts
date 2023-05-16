a7d199b3bff94ae4e08c0b0d6c2a27e85360096c
Kyle Hoondert <kyle.hoondert@imply.io>
2023-05-16 08:37:18 +0100

    Latest report CLI changes

--- src/typescript/cli/src/index.ts
+++ src/typescript/cli/src/index.ts
@@ -3,38 +3,42 @@ const { Command } = require("commander");
 const fs = require("fs");
 const path = require("path");
 const figlet = require("figlet");
-
 const program = new Command();
+
+export {};
 
 console.log(figlet.textSync("Getafix"));
 
 program
   .version("1.0.0")
   .description("A CLI for executing Druid cluster review reports")
-  .option("-i, --inputDir  [value]", "Input directory e.g., /var/tmp/localhost_20230425")
+  .option("-i, --inputDir  [value]", "Input directory e.g., ~/localhost_20230425")
   .parse(process.argv);
-
 const options = program.opts();
 
-async function listDirContents(filepath: string) {
-  try {
-    const files = await fs.promises.readdir(filepath);
-    const detailedFilesPromises = files.map(async (file: string) => {
-      let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
-      return { filename: file };
-    });
-    const detailedFiles = await Promise.all(detailedFilesPromises);
-    console.table(detailedFiles);
-  } catch (error) {
-    console.error("Error occurred while reading the directory!", error);
+async function getJsonFiles(filepath: string): Promise<Record<string, any>> {
+  const files = await fs.promises.readdir(filepath);
+  const jsonFiles = files.filter((file: string) => file.endsWith('.json'));
+  const data: Record<string, any> = {};
+  for (const file of jsonFiles) {
+    const content = await fs.promises.readFile(path.join(filepath, file));
+    const json = JSON.parse(content);
+    data[file] = json;
   }
+  return data;
 }
 
-if (options.inputDir) {
-  const filepath = typeof options.inputDir === "string" ? options.inputDir : __dirname;
-  listDirContents(filepath);
-}
 
-if (!process.argv.slice(2).length) {
+async function main() {
+  let filepath = '.';
+  if (!process.argv.slice(2).length) {
     program.outputHelp();
   }
+  if (options.inputDir) {
+    filepath = typeof options.inputDir === "string" ? options.inputDir : __dirname;
+  }
+  const data = await getJsonFiles(filepath);
+  console.log(data);
+}
+
+main();
