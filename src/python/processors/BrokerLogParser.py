791ec7b5b72ad4f12684b4ca5c9f4e0c6121fbcc
Tijo Thomas <tijoparacka@gmail.com>
2024-10-16 01:16:14 +0530

    Modified to use regex for parsing the brokerlog . A sample regex is included in the code which can parse imply distro broker log parser . regex for OSS log parsing will be updated in the readme.

--- src/python/processors/BrokerLogParser.py
+++ src/python/processors/BrokerLogParser.py
@@ -4,6 +4,7 @@ import csv
 import json
 import argparse
 import datetime
+import re
 
 csv.field_size_limit(sys.maxsize)
 
@@ -11,32 +12,41 @@ def checkOptions():
     parser = argparse.ArgumentParser(description = 'Broker log query parser')
     parser.add_argument('-i', '--inputFile', required=True, action='store', type=str, dest='inputFile', help='Source Broker log file')
     parser.add_argument('-o', '--outputFile', required=True, action='store', dest='outputFile', help='Output csv file')
+    parser.add_argument('-p', '--pattern', required=False, action='store', type=str, dest='pattern', help='Regex pattern for log parsing')
     parser.add_argument('-d', '--debug', action='store_true', dest='debug', help='Extra debugging')
     options = parser.parse_args()
 
     return options
 
-
 def main():
     global options
     options = checkOptions()
+
+    default_pattern = r'^(?P<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2},\d{3})\s+(?P<log_level>\w+)\s+\[(?P<thread>.*?)\]\s+(?P<logger>[^\s]+)\s+-\s+(?P<query_timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z)\s+(?P<ip>\d+\.\d+\.\d+\.\d+)\s+(?P<query>\{.*\})\s+(?P<result>\{.*\})$'
+    log_pattern = re.compile(options.pattern if options.pattern else default_pattern)
 
     with open(options.inputFile, "r") as inFile:
         with open(options.outputFile, "w") as outFile:
             writer = csv.writer(outFile)
             writer.writerow(["eventtime", "querytype", "datasource", "queryid", "priority", "recency", "duration", "queryTime", "queryBytes", "success", "filters", "aggregations", "implyUser", "query"])
-            tsv_file = csv.reader(inFile, delimiter = "\t")
-            for inLine in tsv_file:
-                try:
-                    query = json.loads(inLine[2])
-                    queryResult = json.loads(inLine[3])
-                except:
+            for line in inFile:
+                match = log_pattern.match(line)
+                if not match:
                     continue
                 try:
-                     logTime = inLine[0].split()[5]
-                     logDate = datetime.datetime.strptime(logTime, "%Y-%m-%dT%H:%M:%S.%fZ")
-                except:
-                     continue
+                    query = json.loads(match.group('query'))
+                    queryResult = json.loads(match.group('result'))
+                except json.JSONDecodeError:
+                    continue
+                try:
+                    logTime = match.group('query_timestamp')
+                    logDate = datetime.datetime.strptime(logTime, "%Y-%m-%dT%H:%M:%S.%fZ")
+                except ValueError:
+                    continue
+                try:
+                    query['queryType']
+                except KeyError:
+                    continue    
                 if query['queryType'] == 'segmentMetadata':
                     continue
                 elif query['queryType'] == 'union':
@@ -44,14 +54,14 @@ def main():
                     queryId = query['query']['context']['queryId']
                     try:
                         priority = query['query']['context']['priority']
-                    except:
+                    except KeyError:
                         priority = 'Null'
                     intervals = query['query']['intervals']['intervals']
                 else:
                     if query['dataSource']['type'] == 'table':
                         try:
                             dataSource = query['dataSource']['name']
-                        except:
+                        except KeyError:
                             dataSource = ''
                             for ds in query['dataSource']['dataSources']:
                                 if len(dataSource) > 1:
@@ -63,24 +73,20 @@ def main():
                     queryId = query['context']['queryId']
                     try:
                         priority = query['context']['priority']
-                    except:
+                    except KeyError:
                         priority = 'Null'
-                    
                     intervalStart = 0
                     intervalEnd = 0
-                    
                     try:
                         intervals = query['intervals']['intervals']
                         if intervals and len(intervals) > 0:
                             interval = intervals[0]
                             interval_parts = interval.split('/')
-
                             if len(interval_parts) >= 1:
                                 try:
-                                    intervalStart = datetime.datetime.strptime(interval.split('/')[0], "%Y-%m-%dT%H:%M:%S.%fZ")
+                                    intervalStart = datetime.datetime.strptime(interval_parts[0], "%Y-%m-%dT%H:%M:%S.%fZ")
                                 except ValueError:
                                     pass
-                        
                             if len(interval_parts) >= 2:
                                 try:
                                     intervalEnd = datetime.datetime.strptime(interval.split('/')[1], "%Y-%m-%dT%H:%M:%S.%fZ")
@@ -88,7 +94,6 @@ def main():
                                     pass
                     except (KeyError, IndexError):
                         pass
-
                 filterList = []
                 try:
                     if query['filter']['type'] == 'selector' or query['filter']['type'] == 'regex':
@@ -102,22 +107,23 @@ def main():
                                     filterList.append(filters['dimension'].replace('"',''))
                                 elif filters['type'] == 'not':
                                     filterList.append(filters['field']['dimension'].replace('"',''))
-                            except:
+                            except KeyError:
                                 if options.debug:
                                     print('Filter Exception:', filters)
                                 filterList.append(json.dumps(filters))
                                 continue
-                except:
-                    continue
-                filterList = list(dict.fromkeys(filterList))
+                
+                    filterList = list(dict.fromkeys(filterList))
+                except KeyError:
+                    pass
                 if options.debug:
                     print(str(filterList))
                 aggregationsList = []
                 try:
                     for aggregations in query['aggregations']:
                         aggregationsList.append(aggregations['type'].replace('"',''))
-                except:
-                    continue
+                except KeyError:
+                    pass
                 try:
                     intervalStart = datetime.datetime.strptime(interval.split('/')[0], "%Y-%m-%dT%H:%M:%S.%fZ")
                 except ValueError:
@@ -137,8 +143,7 @@ def main():
                     implyUser = data["context"]["implyUser"]
                 except KeyError:
                     implyUser = ''
-                #outLine = [ logTime, query['queryType'], dataSource, queryId, priority, int(round(recency.total_seconds())), int(round(duration.total_seconds())), queryTime, queryBytes, success, str(filterList), json.dumps(query)]
                 writer.writerow([logTime, query['queryType'], dataSource, queryId, priority, int(round(recency.total_seconds())), int(round(duration.total_seconds())), queryTime, queryBytes, success, (', '.join(filterList)), (', '.join(aggregationsList)), implyUser, json.dumps(query)])
 
 if __name__ == '__main__':
-    main()   
+    main()
